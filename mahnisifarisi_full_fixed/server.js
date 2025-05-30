06fea2444cc0178fd183324608152f78f8c60fc8
AghalarGadirov <aghalar.gadirov@gmail.com>
2025-05-30 06:33:47 +0400

    Add files via upload

--- mahnisifarisi_full_fixed/server.js
+++ mahnisifarisi_full_fixed/server.js
@@ -1,31 +1,58 @@
+
 const express = require("express");
-const fs = require("fs");
 const cors = require("cors");
+const fs = require("fs");
 const app = express();
-const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
-// Qiymet JSON yolu
+// Qiymet endpointləri
 const qiymetPath = "./qiymet.json";
-
-// Qiymet oxu
 app.get("/api/qiymet", (req, res) => {
-    fs.readFile(qiymetPath, "utf8", (err, data) => {
-        if (err) return res.status(500).send("Xəta baş verdi!");
-        res.send(JSON.parse(data));
-    });
+  fs.readFile(qiymetPath, "utf8", (err, data) => {
+    if (err) return res.status(500).send("Qiymətlər oxunmadı!");
+    res.send(JSON.parse(data));
+  });
 });
-
-// Qiymet yaz
 app.put("/api/qiymet", (req, res) => {
-    fs.writeFile(qiymetPath, JSON.stringify(req.body, null, 2), err => {
-        if (err) return res.status(500).send("Yazılmadı!");
-        res.send({ message: "Dəyişiklik yadda saxlanıldı!" });
-    });
+  fs.writeFile(qiymetPath, JSON.stringify(req.body, null, 2), (err) => {
+    if (err) return res.status(500).send("Qiymətlər yazılmadı!");
+    res.send({ message: "Qiymətlər yadda saxlanıldı!" });
+  });
 });
 
-app.listen(port, () => {
-    console.log(`Server işə düşdü: http://localhost:${port}`);
+// Əlaqə endpointləri
+const elaqePath = "./elaqe.json";
+app.get("/api/elaqe", (req, res) => {
+  fs.readFile(elaqePath, "utf8", (err, data) => {
+    if (err) return res.status(500).send("Əlaqə oxunmadı!");
+    res.send(JSON.parse(data));
+  });
+});
+app.put("/api/elaqe", (req, res) => {
+  fs.writeFile(elaqePath, JSON.stringify(req.body, null, 2), (err) => {
+    if (err) return res.status(500).send("Əlaqə yazılmadı!");
+    res.send({ message: "Əlaqə məlumatları yadda saxlanıldı!" });
+  });
+});
+
+// Formlar endpointləri
+const formlarPath = "./formlar.json";
+app.get("/api/formlar", (req, res) => {
+  fs.readFile(formlarPath, "utf8", (err, data) => {
+    if (err) return res.status(500).send("Formları oxumaqda xəta!");
+    res.send(JSON.parse(data));
+  });
+});
+app.put("/api/formlar", (req, res) => {
+  fs.writeFile(formlarPath, JSON.stringify(req.body, null, 2), err => {
+    if (err) return res.status(500).send("Formlar yazılmadı!");
+    res.send({ message: "Formlar yadda saxlanıldı!" });
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server işə düşdü: " + PORT);
 });
